ee3d98aecc755d28802760850ddd525ecb40b8b0
ssbhattarai <nascentsurya@gmail.com>
2022-01-16 21:26:25 +0545

    vuex-setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,23 @@ require('./bootstrap')
 
 
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
+
 import router from './router';
+import { playlist } from './store/playList';
 
 import ExampleComponent from './components/ExampleComponent';
 
 
-const app = createApp({}).use(router);
+const store = createStore({
+	modules: {
+        playlist
+	}
+});
+
+const app = createApp({})
+            .use(router)
+            .use(store);
 
 app.component('example-component', ExampleComponent);
 
